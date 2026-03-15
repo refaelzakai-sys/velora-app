3830332599809687c22ec6e4919d13167eefdbc1
refaelzakai-sys <refaelzakai@gmail.com>
2026-03-15 14:24:16 +0200

    Simplify event listeners in service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,2 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  // מאפשר לאפליקציה לעבוד גם בחיבור איטי
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
